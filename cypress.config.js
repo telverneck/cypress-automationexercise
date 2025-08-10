5c0d08316e2ddb51176f1a94840c15a4826e0cc1
telverneck <telverneck@hotmail.com>
2025-08-10 03:42:20 +0100

    ci: setup gitHub pages for Cypress Report

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,13 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.automationexercise.com",
     specPattern: "cypress/e2e/tests/**/*.cy.js",
-    supportFile: "cypress/support/e2e.js"
+    supportFile: "cypress/support/e2e.js",
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: false,
+      json: true
+    }
   }
 });
